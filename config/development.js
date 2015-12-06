713cdf80e5f215ff4dd207576b3e79fe92610e1d
jazzyarchitects <jazzyarchitects@users.noreply.github.com>
2015-12-06 09:00:40 +0530

    removed ; error

--- config/development.js
+++ config/development.js
@@ -1,8 +1,3 @@
-/*
-    Author : Akas Antony
-    Date : 14/05/2015
-*/
-
 'use strict';
 var path = require('path');
 
@@ -12,7 +7,7 @@ module.exports = {
     root: rootPath,
     db: {
         //uri: 'mongodb://127.0.0.1/remedyShare',
-        uri: ' mongodb://jazzyarchitects:jazzy@architects@ds061354.mongolab.com:61354/heroku_mrwqgl13';
+        uri: ' mongodb://jazzyarchitects:jazzy@architects@ds061354.mongolab.com:61354/heroku_mrwqgl13'
         options: {
             server: {
                 socketOptions: {
