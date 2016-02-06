cca3414d3b7d0ee315b6ae1750903f93c23d3ebc
Jibin Mathews <jibinmathews7@gmail.com>
2016-02-06 18:15:38 +0530

    Server url

--- config/serverConfig.js
+++ config/serverConfig.js
@@ -3,7 +3,7 @@
  */
 module.exports = {
   db:{
-      host: 'mongodb://127.0.0.1/remedyShare'
-      //host: 'mongodb://jibin19596:jazzy_architects@ds061354.mongolab.com:61354/heroku_mrwqgl13'
+      // host: 'mongodb://127.0.0.1/remedyShare'
+      host: 'mongodb://jibin19596:jazzy_architects@ds061354.mongolab.com:61354/heroku_mrwqgl13'
   }
 };
