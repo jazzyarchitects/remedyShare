dcb689af8997c71c5a036706b1e8aae5ca6d07eb
Jazzy Architects <jazzyarchitects@users.noreply.github.com>
2016-02-26 20:02:57 +0530

    MongoDb changed again

--- config/serverConfig.js
+++ config/serverConfig.js
@@ -2,8 +2,6 @@
  * Created by Jibin_ism on 13-Jan-16.
  */
 module.exports = {
-	var dbHost = process.env.OPENSHIFT_MONGODB_DB_HOST;
-	var dbPort = process.env.OPENSHIFT_MONGODB_DB_PORT;
   db:{
        // host: 'mongodb://127.0.0.1/remedyShare'
       // host: 'mongodb://jibin19596:jazzy_architects@ds061354.mongolab.com:61354/heroku_mrwqgl13'
