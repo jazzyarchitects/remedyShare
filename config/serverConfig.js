77f4709c0392ee4ccd47ba825ae224713455657a
Jazzy Architects <jazzyarchitects@users.noreply.github.com>
2016-02-26 18:40:29 +0530

    Server Side Script accidently deleted

---
+++ config/serverConfig.js
@@ -0,0 +1,9 @@
+/**
+ * Created by Jibin_ism on 13-Jan-16.
+ */
+module.exports = {
+  db:{
+       // host: 'mongodb://127.0.0.1/remedyShare'
+      host: 'mongodb://jibin19596:jazzy_architects@ds061354.mongolab.com:61354/heroku_mrwqgl13'
+  }
+};
